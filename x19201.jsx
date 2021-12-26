6d01c9fcbb96e7008c6ead00d0bc1aebfed98268
이설빈 <iseolbin@iseolbin-ui-MacBookPro.local>
2021-12-27 01:10:06 +0900

    first commit

---
+++ x19201.jsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./x19201.css";
+
+class X19201 extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="891a3d86-ec74-4d51-9889-1e046153eed4" className="x19201">        <div data-layer="68f333d1-6011-4f31-9c3e-9abd6a4de671" className="nonspacef99ed03c"><span className="nonspacef99ed03c-0">nonspace</span><span className="nonspacef99ed03c-8"> </span></div>
+        <div data-layer="8e1d7986-9394-4ddf-ae97-acb9df2766eb" className="nonspace"><span className="nonspace-0">nonspace</span><span className="nonspace-8"> </span></div>
+        <div data-layer="375bcd8d-8111-417e-b7c5-151070904259" className="xf56ddc72"></div>
+        <div data-layer="b05f4133-f014-40fc-a24a-d81cc26abe12" className="x1"></div>
+        <div data-layer="7481b6b3-94e5-4cc0-ae4f-fe42384bc681" className="x2"></div>
+        <div data-layer="2c0e9e2c-69a0-4f10-b092-52f5e22badc4" className="apologyGoggleGunwriterFigureFigurePerfumeCandleImagePlateColorPencilNoodlesDollBottleOpenerUnexpectedUnderstanding">Apology Goggle Gunwriter Figure Figure Perfume Candle Image Plate Color Pencil Noodles Doll Bottle Opener Unexpected Understanding</div>
+        <div data-layer="5d49ffdd-38bc-4a06-b4aa-ff58c8a3481c" className="x3"></div>
+        <div data-layer="9aa007fa-9415-4f9d-acdf-58839d7d4dcd" className="thisIsOurCodeCopyTheseWordsAndPasteThemOnWebsToPostPicturesTogetherWeWillDredgeYourImageOnTheWebBasedOnThisCode">This is our code. Copy these words and paste them on webs to post pictures together. <br />We will dredge your image on the web based on this code.</div>
+        <div data-layer="67a1a221-9174-4d6f-b486-3779393f35e7" className="x">이것은 우리의 코드입니다. 이 단어들을 복사하여 다양한 웹에 붙여넣어 사진을 같이 올리세요. <br />우리는 이 코드를 기반으로 당신의 이미지를 웹 속에서 낚시해 올 것입니다. </div>
+</div>
+
+    );
+  }
+}
+
+X19201.propTypes = {
+
+}
+
+X19201.defaultProps = {
+
+}
+
+
+export default X19201;
+          
